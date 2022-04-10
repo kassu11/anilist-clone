f530f5e6ac514e99546787d35f6d53c138ce642b
kassu11 <61124141+kassu11@users.noreply.github.com>
2022-04-10 16:09:37 +0300

    10 - User stats
    - Lisäsin sivun jossa näkyy mitä käyttäjillä on yhteistä niitten anime kattomisissa

--- src/Pages/UsersPage.js
+++ src/Pages/UsersPage.js
@@ -2,13 +2,13 @@ import { useParams } from "react-router";
 import axios from "axios";
 import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
+import MDEditor from '@uiw/react-md-editor';
 
 import "../scss/userPage.scss";
 
 function UsersPage() {
 	const [users, setUsers] = useState([]);
 	const [userAnimes, setUserAnimes] = useState([]);
-
 	const [sameAnime, setSameAnime] = useState([]);
 
 	const query = `
@@ -49,13 +49,39 @@ function UsersPage() {
 				entries {
 					id
 					score
+					progress
+          media {
+            id
+            title {
+							romaji
+							english
+							native
+							userPreferred
+          	}
+						coverImage {
+							large
+						}
+						season
+						seasonYear
+						description
+						startDate {
+							year
+							month
+							day
+						}
+						endDate {
+							year
+							month
+							day
+						}
+					} 
 				}
 			}
 		}
 	}`;
 
 	useEffect(() => {
-		if(users.length == 0) return;
+		if(users.length == 0 || userAnimes.length >= users.length) return;
 		axios.post("https://graphql.anilist.co", {
 			query: mediaQuery,
 			variables: {id: users.at(-1)?.id, type: "ANIME"}
@@ -63,7 +89,7 @@ function UsersPage() {
 			const listIndex = data.MediaListCollection.lists.findIndex(list => list.name == "Completed");
 			userAnimes.push(data.MediaListCollection.lists[listIndex])
 			setUserAnimes([...userAnimes]);
-			console.log(userAnimes)
+			console.log(userAnimes);
 		})
 	}, [users])
 
@@ -76,6 +102,7 @@ function UsersPage() {
 					<input type="text" placeholder="User name" spellCheck="false" onKeyDown={e => {
 						if(e.keyCode === 13) {
 							const search = e.target.value;
+							e.target.value = "";
 							if(search.length > 0) {
 								axios.post("https://graphql.anilist.co", {
 									query: query,
@@ -84,7 +111,6 @@ function UsersPage() {
 									if(users.find(user => user.id === data.User.id)) return;
 									users.push(data.User);
 									setUsers([...users]);
-									console.log(users);
 								}).catch(e => {
 									console.log("Invalid user")
 								})
@@ -129,17 +155,77 @@ function UsersPage() {
 						</div>
 					))}
 				</div>
-				
+
 				{users.length > 0 ? (
-					<div className="compare">
-						<p>Compare stats</p>
-					</div>
+					<>
+						<div className="compare" onClick={e => {
+							if(userAnimes.length == 0) return;
+							if(userAnimes.length == 1) {
+								userAnimes[0].entries.sort((a, b) => b.score - a.score);
+								setSameAnime(userAnimes[0].entries.map(e => ({users: [e]})))
+							} else {
+								const minIndex = userAnimes.reduce((acc, {entries}, i) => {
+									if(acc.length < entries.length) return acc;
+									return {length: entries.length, index: i};
+								}, {length: Infinity, index: 0});
+								const animeList = {};
+
+								userAnimes[minIndex.index].entries.forEach(value => animeList[value.media.id] = []);
+		
+								const allAnimesArray = [];
+		
+								for(let i = 0; i < userAnimes.length; i++) {
+									const length = userAnimes.length - 1
+									const isLast = i == length;
+									if(isLast) {
+										for(const entry of userAnimes[i].entries) {
+											if(animeList[entry.media.id]?.length == length) {
+												animeList[entry.media.id].push(entry);
+												allAnimesArray.push({users: animeList[entry.media.id]})
+											}
+										}
+									} else {
+										for(const entry of userAnimes[i].entries) {
+											animeList[entry.media.id]?.push(entry);
+										}
+									}
+								}
+
+								allAnimesArray.forEach(row => row.avarageScore = row.users.reduce((acc, {score}) => acc + score, 0) / row.users.length);
+								console.log(allAnimesArray)
+								setSameAnime(allAnimesArray.sort((e1, e2) => e2.avarageScore - e1.avarageScore));
+							}
+
+						}}>
+							<p>Compare stats</p>
+						</div>
+						{sameAnime.length ? <p className="totalAnimes">{`Total animes in common: ${sameAnime.length}`}</p> : null}
+					</>
 				): null}
-				
-				<div className="animes">
-					{/* {userAnimes
-						.reduce((acc, list) => Math.min(list.entries.length, acc), userAnimes.length - 1)
-						} */}
+
+				<div className="userAnimes" style={{display: sameAnime.length ? null : "none"}}>
+					{sameAnime.map(({users: allUsersData, avarageScore}, i) => (
+						<div key={allUsersData[0].id} className="anime">
+							<div className="imgContainer">
+								<img src={allUsersData[0].media.coverImage.large}></img>
+								<p className="meanScore">{avarageScore?.toFixed(2) ?? allUsersData[0].score}</p>
+							</div>
+							<div className="info">
+								<p className="title">{allUsersData[0].media.title.english || allUsersData[0].media.title.userPreferred}</p>
+								{allUsersData.length > 1 ? (
+									<div className="userScores">
+										{allUsersData.map((user, i) => (
+											<div key={user.id} className="userScore">
+												<img className="avatar" src={users[i]?.avatar.medium}></img>
+												<p>{users[i]?.name}</p>
+												<p className="score">{user.score}</p>
+											</div>
+										))}
+									</div>
+								) : <MDEditor.Markdown source={allUsersData[0].media?.description} />}
+							</div>
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
